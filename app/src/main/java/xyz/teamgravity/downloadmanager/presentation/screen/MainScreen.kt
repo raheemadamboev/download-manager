7b134971012d119647f3e783217b69685e7d2348
raheemadamboev <vividricemaryandmax@gmail.com>
2023-01-23 14:50:11 +0500

    Built MainScreen.kt;

--- app/src/main/java/xyz/teamgravity/downloadmanager/presentation/screen/MainScreen.kt
+++ app/src/main/java/xyz/teamgravity/downloadmanager/presentation/screen/MainScreen.kt
@@ -1,8 +1,29 @@
 package xyz.teamgravity.downloadmanager.presentation.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import xyz.teamgravity.downloadmanager.R
+import xyz.teamgravity.downloadmanager.presentation.viewmodel.MainViewModel
 
 @Composable
-fun MainScreen() {
-
+fun MainScreen(
+    viewmodel: MainViewModel = hiltViewModel(),
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Button(onClick = viewmodel::onDownload) {
+            Text(text = stringResource(id = R.string.start_downloading))
+        }
+    }
 }
